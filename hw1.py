94bd798ec950c3d1c0adb9f1af012841cb5d7128
minhtcai <minhtc.ai@gmail.com>
2021-10-06 18:04:17 -0400

    remove comment

--- hw1.py
+++ hw1.py
@@ -1,4 +1,3 @@
-# Main branch github
 import argparse
 import os
 import torch
@@ -73,34 +72,9 @@ class MANN(nn.Module):
         dim = self.input_size
         B = len(input_images)
         
-        
-        ### VERIFTY BATCH SHAPE AND LABEL
-        # print('Verify that the input labels [B, K+1, N, N] have query set with label should be 9')
-        # # first batch, last K + 1, first char 
-        # #print(input_labels[0][5][0]) # label should end with 9
-        # # second batch, last K + 1, second char
-        # #print(input_labels[1][5][1]) # label should end with 9
-        # for i in range(B):
-        #     for j in range(N):
-        #         print(input_labels[i][K][j]) # should be 9
-        #### PASSED
-        
-        reshaped_image = input_images
-        reshaped_label = input_labels
-        
-        # reshape tensors to process
-        reshaped_image = torch.reshape(reshaped_image, (B * N * (K+1), dim))
-        reshaped_label = torch.reshape(reshaped_label, (B * N * (K+1), N))
-        
-        # #### VERIFTY BATCH SHAPE AND LABEL
-        # # confirm that after reshape, label of query set go to bottom of batch
-        # print('Verify that the query set at the bottom of the matrix will have last digit of the label should be 9')
-        # for i in range(N):
-        #     print(reshaped_label[B * N * (K+1) - i - 1])
-        # #print(reshaped_label[B * N * (K+1) - 3]) # label should end with 9
-        # #print(reshaped_label[B * N * (K+1) - 2]) # label should end with 9
-        # #print(reshaped_label[B * N * (K+1) - 1]) # label should end with 9
-        # #### PASSED
+        # reshape tensors to process [B * (K+1) * N, dim] and [B * (K+1) * N, N]
+        reshaped_image = torch.reshape(input_images, (B * N * (K+1), dim))
+        reshaped_label = torch.reshape(input_labels, (B * N * (K+1), N))
         
         # conatenate query image with label of zeros
         # Notes: in the previous part, we put all the query set of each character in each batch to the bottom, for each batch we have (1 * N) at the
@@ -114,71 +88,36 @@ class MANN(nn.Module):
             if (i + 1) % (N * (K + 1)) == 0:
                 for j in range(i, i - N, -1):
                     pass
-                    #print(reshaped_label[j]) # should be 9? # correct
                     # get problem with mutability # https://discuss.pytorch.org/t/why-change-a-tensors-value-in-a-function-can-change-the-outer-tensor/94661
                     # https://deep-learning-phd-wiki.readthedocs.io/en/latest/src/code/learnPytorch.html
                     #reshaped_label[j] = torch.zeros(N)
 
+        # Since Torch Tensor is an immutable object, so in this case I convert it to numpy tensor to set query set labels to vector of zeros
         img_test = np.asarray(reshaped_image.cpu())
         label_test = np.asarray(reshaped_label.cpu())
         for i in range(len(label_test)):
             if (i + 1) % (N * (K + 1)) == 0:
                 for j in range(i, i - N, -1):
-                    # print(label_test[j])
                     label_test[j] = np.zeros(N)
-                    # print(label_test[j])
+        
+        # Concatenate image and label vector and reshapre back to [B, (K+1)*N, dim + N]
         cat_test = np.concatenate((img_test, label_test), axis = 1)
         cat_test = np.reshape(cat_test, (B, (K+1) * N, dim + N))
         cat_test = torch.from_numpy(cat_test)
         cat_test = cat_test.to(torch.double)
         cat_test = cat_test.to('cuda')
-        #print(cat_test.is_cuda)
-                    
-        #### VERY BATCH SHAPE AND LABEL
-        # print('Verify that the query set at the bottom of the matrix will have modified label should be zeros')
-        # for i in range(N):
-        #     print(label_test[B * N * (K+1) - i - 1])
-        #### PASSED
 
-        # concatenate image and label to a tensor of [B * (K+1) * N, dim + N]
-        concatenated = torch.cat((reshaped_image, reshaped_label), dim = 1)
-        
-        # reshape concatenated tensor to [B, K+1, N, dim + N]
-        concatenated = torch.reshape(concatenated, (B, (K+1) * N, dim + N))
-        concatenated = concatenated.to(torch.double)
-        
-        # use print to confirm that query label have values of zeros
-        
-        # #### VERY BATCH SHAPE AND LABEL
-        # print('Verify that the query set of reshaped concatenated will have modified label should be zeros')
-        # for i in range(B):
-        #     for j in range((K + 1) * N - N, (K + 1) * N, 1):
-        #         #print(j)
-        #         print(concatenated[i][j][-N:]) # should be zeros
-        # #### PASSED
-        
-        # [B, K+1) * N, dim + N]
-        # print(concatenated[0][40][783:]) # label should be zero 
-        # print(concatenated[0][41][783:]) 
-        # print(concatenated[0][42][783:]) 
-        # print(concatenated[0][43][783:])
-        # print(concatenated[1][40][783:]) # label should be zero 
-        # print(concatenated[1][41][783:]) 
-        # print(concatenated[1][42][783:]) 
-        # print(concatenated[1][43][783:]) 
+        # Pass data to model
         if self.use_dnc == True:
         	output, _ = self.dnc(cat_test.float())
         	return output
         else:
 	        # pass data through model and reshape output the [B, K + 1, N, N]
 	        output, _ = self.layer1(cat_test)
-	        #output, _ = self.layer1(concatenated)
 	        output, _ = self.layer2(output)
 	        output = torch.reshape(output, (B, K + 1, N, N))
-	        #output = output.to(torch.float)
-	        #print(output.shape)
         
-        return output.to(torch.float)
+       		return output.to(torch.float)
 
 
     def loss_function(self, preds, labels):
@@ -205,32 +144,9 @@ class MANN(nn.Module):
         dim = self.input_size
         B = len(labels)
         
-        
-        #### VERY BATCH SHAPE AND LABEL
-        # print('Verify labels value in label tensor pre-reshape, should be 9')
-        # for i in range(B):
-        #     for j in range(N):
-        #         print(labels[i][K][j]) # should be 9
-        #print(labels[0][10][0])
-        #print(labels[0][10][1])
-        #print(labels[0][10][2])
-        
         # Reshape two inputs into [B * (K+1) * N, N]
         reshaped_preds = torch.reshape(preds, (B * (K+1) * N, N))
         reshaped_labels = torch.reshape(labels, (B * (K+1) * N, N))
-        
-        ##### VERY BATCH SHAPE AND LABEL
-        # print(labels.shape)
-        # print('Verify labels value in label tensor, should be 9')
-        # print(reshaped_labels[40]) # label should be 9 
-        # print(reshaped_labels[41]) 
-        # print(reshaped_labels[42]) 
-        # print(reshaped_labels[43])
-        # print(reshaped_labels[84]) # label should be 9 
-        # print(reshaped_labels[85]) 
-        # print(reshaped_labels[86]) 
-        # print(reshaped_labels[87]) 
-        # ##
         
         # Get prediction and label from the last items, should be  last N * 1 sample 
         preds_N = reshaped_preds[(len(reshaped_preds) -  B*N):, :]
@@ -244,7 +160,6 @@ class MANN(nn.Module):
         
         output = F.cross_entropy(preds_N, labels_N)
         
-        #print(output)
         return output    
         
 
